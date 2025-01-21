4bcf5dd51ad86e2eb9ecd98329d491aaceb99589
ukey77 <yujin25683@gmail.com>
2025-01-21 14:28:36 +0900

    palindrome update

--- 202501/0121_화/palindrome.ts
+++ 202501/0121_화/palindrome.ts
@@ -1,52 +1,52 @@
-// class PalindromeControl{
-//     protected id: string;
-//     constructor(id: string){
-//         this.id = id;
-//     }
-//     palindrome(data: string): string[]{
-//         const dataArr: string[] = [...data];
-//         const resultArr: string[] = [];
-//         class Stack<T>{
-//             protected id: string;
-//             public storage: Array<T>;
-//             constructor(id: string){
-//                 this.id=id;
-//                 this.storage = [];
-//             }
-//             pushItem(item: T){
-//                 this.storage.push(item);
-//             }
-//             popItem(): T | undefined{
-//                 return this.storage.pop();
-//             }
-//             get length(){
-//                 const length = this.storage.reduce((acc,cur)=>{return acc+=1;},0);
-//                 return length;
-//             }
-//         }
+class PalindromeControl{
+    protected id: string;
+    constructor(id: string){
+        this.id = id;
+    }
+    palindrome(data: string){
+        const dataArr: string[] = [...data];
+        const resultArr: any[] = [];
+        class Stack<T>{
+            protected id: string;
+            public storage: Array<T>;
+            constructor(id: string){
+                this.id=id;
+                this.storage = [];
+            }
+            pushItem(item: T){
+                this.storage.push(item);
+            }
+            popItem(): T | undefined{
+                return this.storage.pop();
+            }
+            get length(){
+                const length = this.storage.reduce((acc,cur)=>{return acc+=1;},0);
+                return length;
+            }
+        }
         
-//         const stack: Stack<string>= new Stack<string>("stack");
+        const stack: Stack<string>= new Stack<string>("stack");
 
-//         dataArr.forEach((item)=>{
-//             stack.pushItem(item);
-//         });
+        dataArr.forEach((item)=>{
+            stack.pushItem(item);
+        });
         
-//         while(stack.length > 0){
-//             resultArr.push(stack.popItem());
-//         }
-//         this.outputResult(resultArr)
-//     }
-//     outputResult(resultArr: string[]){
-//         const convertString = "".concat(...resultArr);
-//         console.log(convertString);
-//     }
-//     run(data: string){
-//         this.palindrome(data);
-//     }
-// }
+        while(stack.length > 0){
+            resultArr.push(stack.popItem());
+        }
+        this.outputResult(resultArr);
+    }
+    outputResult(resultArr: string[]){
+        const convertString = "".concat(...resultArr);
+        console.log(convertString);
+    }
+    run(data: string){
+        this.palindrome(data);
+    }
+}
 
 
-// const palin: PalindromeControl = new PalindromeControl("palin");
-// palin.run("ABCDEF");
+const palin: PalindromeControl = new PalindromeControl("palin");
+palin.run("ABCDEF");
 
-// export {}
+export {}
